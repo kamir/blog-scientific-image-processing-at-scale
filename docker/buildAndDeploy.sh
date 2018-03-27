f8bbaab432521011f9d4b4ee4f1700cc1004591a
Mirko Kämpf <mirko.kaempf@cloudera.com>
2018-03-27 17:32:14 +0200

    Update buildAndDeploy.sh

--- docker/buildAndDeploy.sh
+++ docker/buildAndDeploy.sh
@@ -1,25 +1,25 @@
-docker build -t kamir/cdsw-base-4-with-maven-ij-rosbag-extractor . 
+docker build -t kamir/cdsw-base-4-with-maven-ij-rosbag-extractor-v2 . 
 
-docker push kamir/cdsw-base-4-with-maven-ij-rosbag-extractor:latest
+docker push kamir/cdsw-base-4-with-maven-ij-rosbag-extractor-v2:latest
 
-export T=$(date +%I_%M_%S)
-echo "current time is: "$T
+#export T=$(date +%I_%M_%S)
+#echo "current time is: "$T
 
 
 
-docker image ls
+#docker image ls
 
-docker run -it -d --name container_$T kamir/cdsw-base-4-with-maven-ji-rosbag-extractor
+#docker run -it -d --name container_$T kamir/cdsw-base-4-with-maven-ji-rosbag-extractor
 
-docker container ls
+#docker container ls
 
-read -p "Container-ID to connect to : " C_ID
+#read -p "Container-ID to connect to : " C_ID
 
-echo $C_ID 
+#echo $C_ID 
 
-docker exec -i -t $C_ID /bin/bash
+#docker exec -i -t $C_ID /bin/bash
 
-docker container stop $C_ID
+#docker container stop $C_ID
 
 
 
